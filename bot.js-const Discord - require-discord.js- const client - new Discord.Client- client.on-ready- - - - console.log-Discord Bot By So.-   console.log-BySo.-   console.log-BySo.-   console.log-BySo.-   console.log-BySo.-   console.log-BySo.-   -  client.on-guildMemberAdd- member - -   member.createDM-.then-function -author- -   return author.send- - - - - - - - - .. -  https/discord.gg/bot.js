e49f0e3e371eaade799ecf7c226ae1c6badb5b76
Simple <43014320+TofekHamodii@users.noreply.github.com>
2018-11-12 17:29:15 +0200

    Create bot.js

---
+++ bot.js-const Discord - require-discord.js- const client - new Discord.Client- client.on-ready- - - - console.log-Discord Bot By So.-   console.log-BySo.-   console.log-BySo.-   console.log-BySo.-   console.log-BySo.-   console.log-BySo.-   -  client.on-guildMemberAdd- member - -   member.createDM-.then-function -author- -   return author.send- - - - - - - - - .. -  https/discord.gg/bot.js
@@ -0,0 +1,21 @@
+﻿const Discord = require("discord.js");
+const client = new Discord.Client();
+client.on('ready', () => {
+console.log("Discord Bot By So.?");
+  console.log('BySo.?')
+  console.log('BySo.?')
+  console.log('BySo.?')
+  console.log('BySo.?')
+  console.log('BySo.?')
+
+
+});
+
+client.on("guildMemberAdd", member => {
+  member.createDM().then(function (author) {
+  return author.send(' ** حرام واحد جميل مثلك م يكون شايف قناتي .. تنورني  https://youtu.be/5N9ihv2c-5M ** ')
+
+}).catch(console.error)
+})
+
+client.login("NDk5NjAxODEzMzc3Mzg0NDYx.Dssnng.RnfRLh2pxc-X1Vvgwl0LF70yl8k");
